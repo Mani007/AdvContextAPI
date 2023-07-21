e2d93a7c791f68a6c66219ad1f6a95ef2661a8dc
Mohnish <mohnish.a007@gmail.com>
2023-07-21 02:40:37 +0100

    Basic context API implemented, CreateContext, Provider and Finally Conumer of context

--- contextapi/src/App.js
+++ contextapi/src/App.js
@@ -1,14 +1,19 @@
+import React, { createContext } from 'react'
 import ComA from './components/ComA';
 import './App.css';
 
+const firstname = createContext()
 function App() {
   return (
     <div className="App">
       <h3>Hello world!</h3> <br /> <br />
-      <ComA/>
+      <firstname.Provider value={'Jacob'}>
+        <ComA/>
+      </firstname.Provider>
 
     </div>
   );
 }
 
 export default App;
+export {firstname}
--- contextapi/src/components/ComC.jsx
+++ contextapi/src/components/ComC.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { firstname } from '../App'
+// The Conusmer api will always take function
 function ComC() {
   return (
-    <div>ComC</div>
+    <firstname.Consumer>
+    {(fname) =>{
+        return (<div>From ComC <br /> <h4 >The First Name from App context is <span style={{color: 'blue'}}> {fname}</span></h4></div>)
+    }}
+    </firstname.Consumer>
   )
 }
 
